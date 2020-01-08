1ecc11a9cc03519ee24beb81cb1418e5f05d4465
Danzel Rana <danzelrana@gmail.com>
2020-01-08 11:06:13 -0800

    bcrypt

--- server.js
+++ server.js
@@ -29,9 +29,10 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  const { reqEmail, reqPassword } = req.body;
-  const { dbEmail, dbPassword } = database.users[0];
-  if (reqEmail === dbEmail && reqPassword === dbPassword) {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
     res.json("success");
   } else {
     res.status(400).json("error logging in");
@@ -51,16 +52,31 @@ app.post("/register", (req, res) => {
   res.json(database.users[database.users.length - 1]);
 });
 
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) res.status(400).json("not found");
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) res.status(400).json("not found");
+});
+
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
-
-/*
-
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userID  --> GET = user
-/image --> PUT --> user
-
-*/
