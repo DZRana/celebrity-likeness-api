cb7cb67a9011a1783afd2f45c4b7bc3f96192aa8
Danzel Rana <danzelrana@gmail.com>
2020-01-08 12:10:11 -0800

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const cors = require("cors");
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 const database = {
   users: [
