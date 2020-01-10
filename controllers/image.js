8dd69c0fe3ebedcaca83a1596bc585a44417c1bb
Danzel Rana <danzelrana@gmail.com>
2020-01-10 13:32:14 -0800

    moved api key to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "af1cb253e4934db3a7e78a48e0e0c5ca"
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.CELEBRITY_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json("unable to work with API"));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
@@ -11,5 +26,6 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-  handleImage
+  handleImage,
+  handleApiCall
 };
